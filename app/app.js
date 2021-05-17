4b3f36c8a73fd73a4d76b7ded8d0828124fd6b0c
kamilpinas <kamilpinas@gmail.com>
2021-05-17 09:26:15 +0200

    done

--- app/app.js
+++ app/app.js
@@ -3,22 +3,48 @@ import config from './config';
 import cors from 'cors';
 import express from 'express';
 import morgan from 'morgan';
+import routes from './REST/routes';
+import mongoose from 'mongoose';
 
 const app = express();
 app.use(express.static(__dirname + '/public'));
 
 app.use(morgan('dev'));
-app.use(bodyParser.urlencoded({extended: false}));
-app.use(bodyParser.json({limit: '2048kb'}));
+app.use(bodyParser.urlencoded({
+    extended: false
+}));
+app.use(bodyParser.json({
+    limit: '2048kb'
+}));
 
 app.use(express.static('public'));
-
 app.use(cors());
 
-app.get('/*', function (req, res) {
-  res.sendFile(__dirname + '/public/index.html');
+mongoose.connect(config.databaseUrl, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false
+}, (error) => {
+    if (error) {
+        console.error(error);
+    } else {
+        console.info('Connect with database established');
+    }
 });
 
-app.listen(config.port, function () {
-  console.info(`Server is running at ${config.port}`)
+process.on('SIGINT', () => {
+    mongoose.connection.close(function() {
+        console.error('Mongoose default connection disconnected through app termination');
+        process.exit(0);
+    });
+});
+
+routes(app);
+
+app.get('/*', function(req, res) {
+    res.sendFile(__dirname + '/public/index.html');
+});
+
+app.listen(config.port, function() {
+    console.info(`Server is running at ${config.port}`)
 });
